17bc6cbacb5ee5eb8f38eb14363f159e4e6833f6
Sahil Dhar <sahildhar98@gmail.com>
2021-10-02 05:25:36 +0530

    logic

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 var billAmt = document.querySelector("#bill-amt");
-var cashgiven = document.querySelector("#cash-given");
+var cashGiven = document.querySelector("#cash-given");
 var btnCheck = document.querySelector("#check-btn");
+var message = document.querySelector("#error-msg");
 
-btnCheck.addEventListener("click", function validateAmount()){
-    if(billAmt)
+btnCheck.addEventListener("click", function validateAmount(){
+    hideMessage();
+    if(billAmt.value > 0){
+        if(cashGiven.value >= billAmt.value){
+            returnAmt = cashGiven.value-billAmt.value;
+            changecalculator(returnAmt);
+
+        }else{
+            showMessage(
+                "The cash provided must be equal to or more than bill amount"
+            );
+        }
+    }else{
+        showMessage("Invalid Bil Amount");
+    }
+});
+
+function hideMessage(){
+    message.style.display="none";}
+
+
+function showMessage(msg){
+    message.style.display ="block";
+    message.innerText =  msg;
 }
