bf9383501fd35edbc4879e0a18727229c2133b72
Sahil Dhar <sahildhar98@gmail.com>
2021-10-02 04:45:10 +0530

    wired button

---
+++ app.js
@@ -0,0 +1,7 @@
+var billAmt = document.querySelector("#bill-amt");
+var cashgiven = document.querySelector("#cash-given");
+var btnCheck = document.querySelector("#check-btn");
+
+btnCheck.addEventListener("click", function validateAmount()){
+    if(billAmt)
+}
